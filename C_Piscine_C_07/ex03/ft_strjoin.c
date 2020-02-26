1d166f48cf8ab4e228bf825ff1d56f6bbfbddd9d
Fuhaang <50319415+Fuhaang@users.noreply.github.com>
2020-02-26 23:03:00 +0100

    correct_ex03

--- C_Piscine_C_07/ex03/ft_strjoin.c
+++ C_Piscine_C_07/ex03/ft_strjoin.c
@@ -11,31 +11,32 @@
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <stdio.h>
 
 int		ft_strlen(char *str)
 {
 	int i;
 
 	i = 0;
-	while (str[i])
+	while (str[i] != '\0')
 		i++;
 	return (i);
 }
 
-int		ft_allstrlen(int size, char **strs, char *sep)
+int		ft_all_strlen(int size, char **strs, char *sep)
 {
-	int		allsize;
-	int		i;
+	int lengh;
+	int i;
 
-	allsize = 0;
 	i = 0;
+	lengh = 0;
 	while (i < size)
 	{
-		allsize += ft_strlen(strs[i]);
+		lengh = lengh + ft_strlen(strs[i]);
 		i++;
 	}
-	allsize = allsize + (size - 1) * ft_strlen(sep);
-	return (allsize);
+	lengh = lengh + (size - 1) * ft_strlen(sep);
+	return (lengh);
 }
 
 char	*ft_strcat(char *dest, char *src)
@@ -45,14 +46,15 @@ char	*ft_strcat(char *dest, char *src)
 
 	i = 0;
 	j = 0;
-	while (dest[i])
+	while (dest[i] != '\0')
 		i++;
-	while (src[j])
+	while (src[j] != '\0')
 	{
-		dest[i + j] = src[j];
+		dest[i] = src[j];
+		i++;
 		j++;
 	}
-	dest[i + j] = '\0';
+	dest[i] = '\0';
 	return (dest);
 }
 
@@ -62,17 +64,18 @@ char	*ft_strjoin(int size, char **strs, char *sep)
 	char	*str;
 
 	i = 0;
-	if (!(str = malloc(sizeof(char) * (ft_allstrlen(size, strs, sep) + 1))))
-		return (NULL);
-	str[0] = '\0';
 	if (size == 0)
-		return (str);
-	while (i < size - 1)
+		return (0);
+	if (!(str = malloc(sizeof(char) * (ft_all_strlen(size, strs, sep) + 1))))
+		return (NULL);
+	i = 0;
+	str[0] = '\0';
+	while (i < size)
 	{
 		ft_strcat(str, strs[i]);
-		ft_strcat(str, sep);
+		if (i < (size - 1))
+			ft_strcat(str, sep);
 		i++;
 	}
-	ft_strcat(str, strs[i]);
 	return (str);
 }
